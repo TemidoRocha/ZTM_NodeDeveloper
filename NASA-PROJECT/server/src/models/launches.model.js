b14923dbfd555c35fd1956f60b9855c7f7ad781d
TemidoRocha <manueltemidorocha@gmail.com>
2022-06-13 20:28:11 +0100

    #NASA-PROJECT #CREATE NEW LAUNCH CONNECTION TO FE

--- NASA-PROJECT/server/src/models/launches.model.js
+++ NASA-PROJECT/server/src/models/launches.model.js
@@ -7,7 +7,7 @@ const launch = {
   mission: 'Keppler Exploration X',
   rocket: 'Explorer IS1',
   launchDate: new Date('Decembe 27, 2030'),
-  destination: 'Kepler-442 b',
+  target: 'Kepler-442 b',
   customer: ['NASA', 'ZTM'],
   upcoming: true,
   success: true,
