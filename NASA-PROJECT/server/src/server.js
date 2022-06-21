77701827cf89f38065f6b5ea93cb294b87f906f2
TemidoRocha <manueltemidorocha@gmail.com>
2022-06-21 20:46:08 +0100

    #NASA-PROJECT #MONGODB JEST IMPLEMENTATION
    Connectino of the test with the DB.
    NOT VIABLE FOR PRODUCTION

--- NASA-PROJECT/server/src/server.js
+++ NASA-PROJECT/server/src/server.js
@@ -1,25 +1,16 @@
-require('dotenv').config();
 const http = require('http');
-const mongoose = require('mongoose');
 
 const app = require('./app');
 
+const { mongoConnect } = require('./services/mongo');
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-mongoose.connection.on('open', () => {
-  console.log('MongoDB connection ready!');
-});
-
-mongoose.connection.on('error', (err) => {
-  console.error(err);
-});
-
 async function startServer() {
-  await mongoose.connect(process.env.MONGODB_URL);
+  await mongoConnect();
   await loadPlanetsData();
 
   server.listen(PORT, () => {
